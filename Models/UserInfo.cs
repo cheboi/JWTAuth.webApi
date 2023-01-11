da09ce2f52c2a4f4c74288df42e5d24b33dde2ce
cheboi <moscheb96@gmail.com>
2023-01-11 11:08:45 +0300

    Add project files.

---
+++ Models/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace JWTAuth.webApi.Models
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string? DisplayName { get; set; }
+        public string?  USerName { get; set; }
+        public string? Email  { get; set; }
+        public string? Password { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
---
+++ SqlQuery/query.sql
@@ -0,0 +1,34 @@
+﻿USE [JWTAuthentication]
+
+CREATE TABLE [dbo].[UserInfo](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [varchar](60) NOT NULL,
+	[UserName] [varchar](30) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Password] [varchar](20) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_UserInfo] PRIMARY KEY CLUSTERED
+	(
+		[UserId] ASC
+	)
+)
+
+CREATE TABLE [dbo].[Employee](
+	[EmployeeID] [int] NOT NULL,
+	[NationalIDNumber] [nvarchar](15) NOT NULL,
+	[EmployeeName] [nvarchar](100) NULL,
+	[LoginID] [nvarchar](256) NOT NULL,
+	[JobTitle] [nvarchar](50) NOT NULL,
+	[BirthDate] [date] NOT NULL,
+	[MaritalStatus] [nchar](1) NOT NULL,
+	[Gender] [nchar](1) NOT NULL,
+	[HireDate] [date] NOT NULL,
+	[VacationHours] [smallint] NOT NULL,
+	[SickLeaveHours] [smallint] NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED
+	(
+		[EmployeeID] ASC
+	)
+)
