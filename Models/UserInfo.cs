93a4054d084a53928f98eb20669542ff68c1d956
cheboi <moscheb96@gmail.com>
2023-01-12 05:52:41 +0300

    Adding The Token

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -4,7 +4,7 @@
     {
         public int UserId { get; set; }
         public string? DisplayName { get; set; }
-        public string?  USerName { get; set; }
+        public string?  UserName { get; set; }
         public string? Email  { get; set; }
         public string? Password { get; set; }
         public DateTime? CreatedDate { get; set; }
